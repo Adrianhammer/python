7275f946ee235f4380e24e12e9b2cf931d3e16d7
Adrianhmmr <adrian10hammer@gmail.com>
2025-03-04 22:09:08 +0100

    gjort opg 1 & 2 til gruppeoppgave for å vise

---
+++ Group assignment/Section_1.py
@@ -0,0 +1,26 @@
+#Section 1 (Weightage: 15 Percentage)
+import random
+
+#1
+"""
+userInput = int(input("How many drinks do you usually have on a night out? "))
+
+if userInput <= 3:
+    print("Weakling")
+elif userInput in range(4,11):
+    print("Not bad...")
+elif userInput > 10:
+    print("Viking!")    
+"""
+
+#2
+
+
+for i in range(1,21):
+    if i % 2 == 0:
+        print(f"{i}")
+
+
+#3
+
+for i in range 
