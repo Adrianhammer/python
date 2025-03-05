0867bbf95d8c595af391ab456c5453ca9484fa3e
Kamillakjaer <kamill211000@gmail.com>
2025-03-05 22:43:31 +0100

    tester push via terminal

--- Group assignment/Section_2.py
+++ Group assignment/Section_2.py
@@ -1,6 +1,8 @@
 # Nå begynner jeg med del 2 av oppgaven
 #2
-# a) - data cleaning
+
+# a) - data cleaning oppgave
 text_file = open("Group assignment/xdc_companies_mac.txt", "r")
 
 print(text_file.read())
+
