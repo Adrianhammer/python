931aff455faaad8634df327e181bde16aada74ed
Kamillakjaer <kamill211000@gmail.com>
2025-03-05 23:01:29 +0100

    oppgave 1 seksjon 2

--- Group assignment/Section_2.py
+++ Group assignment/Section_2.py
@@ -4,5 +4,19 @@
 # a) - data cleaning oppgave
 text_file = open("Group assignment/xdc_companies_mac.txt", "r")
 
-print(text_file.read())
 
+isin_company = []
+marketValue = []
+currency = []
+assetClass = []
+xdc = []
+
+for line in text_file:
+    column = line.split(",")
+    isin_company.append(column[0])
+    marketValue.append(column[1])
+    currency.append(column[2])
+    assetClass.append(column[3])
+    xdc.append(column[4])
+
+print(*isin_company, sep="\n ")
