3727f98ad1167ca784d60a6d44fa2b29822dbc72
Adrianhammer <adrian10hammer@gmail.com>
2025-03-06 15:37:56 +0100

    testet ut noe på oppgave 3

--- Group assignment/Section_2.py
+++ Group assignment/Section_2.py
@@ -2,7 +2,6 @@
 #2
 
 # a) - data cleaning oppgave
-text_file = open("Group assignment/xdc_companies_mac.txt", "r")
 
 
 isin_company = []
@@ -10,13 +9,71 @@ marketValue = []
 currency = []
 assetClass = []
 xdc = []
+isin = []
+company = []
 
-for line in text_file:
-    column = line.split(",")
-    isin_company.append(column[0])
-    marketValue.append(column[1])
-    currency.append(column[2])
-    assetClass.append(column[3])
-    xdc.append(column[4])
+lists = isin_company, marketValue, currency, assetClass, xdc, isin, company
 
-print(*isin_company, sep="\n ")
+
+def function_a():
+    text_file = open("Group assignment/xdc_companies_mac.txt", "r")
+
+    for line in text_file:
+        column = line.split(",")
+        isin_company.append(column[0])
+        marketValue.append(column[1])
+        currency.append(column[2])
+        assetClass.append(column[3])
+        xdc.append(column[4])
+
+    text_file.close()
+    print(*isin_company, sep="\n ")
+
+
+# b)
+
+
+
+def function_b(value):
+    with open("Group assignment/xdc_companies.txt", "r") as text_file:
+        content = text_file.read().replace("_", ",")
+        lines = content.splitlines()
+
+        for line in lines:
+            column = line.split(",")
+            isin.append(column[0])
+            company.append(column[1])
+            marketValue.append(column[2])
+            currency.append(column[3])
+            assetClass.append(column[4])
+            xdc.append(column[5])
+
+
+    #print(*isin, sep="\n ")
+    #print(*company, sep="\n ")
+
+
+# c)
+def function_c():
+    with open("Group assignment/xdc_companies.txt", "r") as text_file:
+        content = text_file.read().replace("_", ",").replace("°C", "")
+        lines = content.splitlines()
+
+        for line in lines:
+            column = line.split(",")
+            isin.append(column[0])
+            company.append(column[1])
+            marketValue.append(column[2])
+            currency.append(column[3])
+            assetClass.append(column[4])
+            xdc.append(column[5])
+
+
+    with open("Group assignment/xdc.txt", "w") as new_file:
+        for list in lists:
+            for word in list:
+                new_file.write("%s\n" % word)
+
+    #print(*value, sep="\n ")
+
+function_c()
