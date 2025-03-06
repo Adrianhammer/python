2ca92757891fc2635024e35a77d103f22861556c
Adrian Hammer <adrian10hammer@gmail.com>
2025-03-06 21:38:16 +0100

    gjort litt mer på gruppeoppgave

--- Group assignment/Section_2.py
+++ Group assignment/Section_2.py
@@ -12,7 +12,7 @@ xdc = []
 isin = []
 company = []
 
-lists = isin_company, marketValue, currency, assetClass, xdc, isin, company
+lists = isin, company, marketValue, currency, assetClass, xdc
 
 
 def function_a():
@@ -27,14 +27,12 @@ def function_a():
         xdc.append(column[4])
 
     text_file.close()
-    print(*isin_company, sep="\n ")
+    #print(*isin_company, sep="\n ")
 
 
 # b)
 
-
-
-def function_b(value):
+def function_b():
     with open("Group assignment/xdc_companies.txt", "r") as text_file:
         content = text_file.read().replace("_", ",")
         lines = content.splitlines()
@@ -53,7 +51,7 @@ def function_b(value):
     #print(*company, sep="\n ")
 
 
-# c)
+# c) Fikser denne senere slik at text filen blir bedre skrevet til (vertikale kolonner istedenfor horisontale)
 def function_c():
     with open("Group assignment/xdc_companies.txt", "r") as text_file:
         content = text_file.read().replace("_", ",").replace("°C", "")
@@ -69,11 +67,32 @@ def function_c():
             xdc.append(column[5])
 
 
+def function_d():
     with open("Group assignment/xdc.txt", "w") as new_file:
         for list in lists:
-            for word in list:
-                new_file.write("%s\n" % word)
+            new_file.write(f"{list}\r\n")
 
-    #print(*value, sep="\n ")
-
+#function_a()
+#function_b()
 function_c()
+function_d()
+# e) oppgave a, b og c er hver sin egen funksjon. Kan sikkert endre på dette senere og ha alt i en funksjon
+
+# Data analysis tool
+
+# f) Hva mener han med denne oppgaven? Mener han lese fra filen vi lagde i oppgave d? eller fra listene
+#    "[...] in your data" er ganske vagt
+
+# g) 
+
+def function_g():
+    user_input = input("Which company would you like to see the data for? ")
+
+    while user_input not in company:
+        print("Please write another company")
+        user_input = input("Which company would you like to see the data for? ")
+    print("great")
+    
+
+
+function_g() 
